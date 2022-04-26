13cbb9e3c5ce358782988a8fc28fc45bb9c84aa3
WataruNishimura <wataru.nishimura@kraftsman.jp>
2022-04-27 00:39:10 +0900

    feat: calculate distanec

--- domain/model/place.go
+++ domain/model/place.go
@@ -24,3 +24,16 @@ func (p *Place) FromSearchResult(result maps.PlacesSearchResult) *Place {
 	p.Address = result.FormattedAddress
 	return p
 }
+
+func PlaceFromSearchResult(result maps.PlacesSearchResult) *Place {
+	var p *Place = &Place{}
+	p.Name = result.Name
+	if len(result.Photos) != 0 {
+		p.ImageUrl = fmt.Sprintf("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=%s&key=%s", result.Photos[0].PhotoReference, os.Getenv("GMAP_API_KEY"))
+	} else {
+		p.ImageUrl = "https://placehold.jp/400x300.png"
+	}
+	p.Address = result.FormattedAddress
+	p.Location = Location{Latitude: result.Geometry.Location.Lat, Longitude: result.Geometry.Location.Lng}
+	return p
+}
