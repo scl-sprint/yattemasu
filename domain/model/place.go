5bf289faefc4d7ef8986940caeb3c8e2410f0d3b
WataruNishimura <wataru.nishimura@kraftsman.jp>
2022-05-07 16:02:38 +0900

    change placeholder

--- domain/model/place.go
+++ domain/model/place.go
@@ -31,7 +31,7 @@ func PlaceFromSearchResult(result maps.PlacesSearchResult) *Place {
 	if len(result.Photos) != 0 {
 		p.ImageUrl = fmt.Sprintf("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=%s&key=%s", result.Photos[0].PhotoReference, os.Getenv("GMAP_API_KEY"))
 	} else {
-		p.ImageUrl = "https://placehold.jp/400x300.png"
+		p.ImageUrl = "https://yattemasu.herokuapp.com/placeholder.png"
 	}
 	p.Address = result.FormattedAddress
 	p.Location = Location{Latitude: result.Geometry.Location.Lat, Longitude: result.Geometry.Location.Lng}
